e8d778b8ac25d87c1be8a11dfe9971671d097890
enchantedcreature <enchantedcreature@outlook.com>
2026-01-05 23:51:21 +0000

    api/redgifs: avoid silent videos

--- api/src/processing/services/redgifs.js
+++ api/src/processing/services/redgifs.js
@@ -17,10 +17,14 @@ export default async function({ id }) {
         html.split("<script type='application/ld+json'>")[1].split('</script>')[0]
     );
 
-    const id_cased = streamData.video.contentUrl.split("/").pop().split(".")[0];	
+    const url_parts = streamData.video.contentUrl.split("/");
+    let [id_cased, extension] = url_parts.slice(-1)[0].split(".");
+    if (id_cased.endsWith("-silent")) {
+        id_cased = id_cased.slice(0,-7);
+    }
 
     return {
-        urls: streamData.video.contentUrl,
+        urls: url_parts.slice(0,-1).join("/") + "/" + id_cased + "." + extension,
         filenameAttributes: {
             service: 'redgifs',
             id: id_cased,
